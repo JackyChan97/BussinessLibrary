e9ac76036c60128533e4163816dada3a3942d2fe
unknown <870103948@qq.com>
2019-06-17 19:33:33 +0800

    django

---
+++ bussinessLibrary/index/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=128,unique=True)
+    url = models.URLField(max_length=200,unique=True)
+    time = models.DateTimeField()
+
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ["time"]
+        verbose_name = "项目"
+        verbose_name_plural = "项目"
+
+class KeyWord(models.Model):
+    keyword = models.CharField(max_length=128,unique=True)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ["keyword"]
+        verbose_name = "关键词"
+        verbose_name_plural = "关键词"
