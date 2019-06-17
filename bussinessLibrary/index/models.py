982c4d05f7e59c3df851e8aaa658655dbb88d0f6
unknown <870103948@qq.com>
2019-06-17 20:58:53 +0800

    email

--- bussinessLibrary/index/models.py
+++ bussinessLibrary/index/models.py
@@ -23,3 +23,12 @@ class KeyWord(models.Model):
         ordering = ["keyword"]
         verbose_name = "关键词"
         verbose_name_plural = "关键词"
+
+class Email(models.Model):
+    emailAddress = models.EmailField(max_length=128,unique=True)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = "邮箱"
+        verbose_name_plural="邮箱"
--- bussinessLibrary/index/views.py
+++ bussinessLibrary/index/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+from email.mime.text import MIMEText
+from email.header import Header
+import smtplib
+import time
 # Create your views here.
+
+def SendEmail(request):
+    with open("pw.txt",'r') as f:
+        text = f.read()
+        text = text.split("abc123")
+    from_add = text[0]
+    password = text[1]
+    add_list = []
+    content = "test"
+    port = 465
+    mail_server = "smtp.qq.com"
+    message = MIMEText(content,"plain","utf-8")
+    message['From'] = Header("BLSYS","utf-8")
+    message['To'] = Header("Customer","utf-8")
+    message['Subject'] = Header(time.strftime("%Y-%m-%d")+"商机","utf-8")
+
+    try:
+        mail = smtplib.SMTP_SSL(mail_server,port)
+        status = mail.login(from_add,password)
+        print(status)
+        mail.sendmail(from_add,add_list,message.as_string())
+        print("邮件发送成功")
+        mail.quit()
+    except:
+        print("发送失败")
+
+
